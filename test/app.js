ea310c95b6c92e4b65dcda9d46a81fa617c9337c
Nerya Sheetrit <neryasheetrit@gmail.com>
2021-06-16 16:20:10 +0300

    more updates

--- test/app.js
+++ test/app.js
@@ -8,25 +8,6 @@ const MashovAPI = require("../index.js");
       semel = allSchools[i].semel;
     }
   }
-  console.log(await MashovAPI.loginWithCreds({username:"328089131",password:"vp8816881688",year:2021,school:441196}));
-  // await MashovAPI.getGrades()    V
-  // await MashovAPI.getPhoto()     V
-  // await MashovAPI.getSchools()   V
-  // await MashovAPI.getBehave()    V
-  // await MashovAPI.getOutBehave() V
-  // await MashovAPI.getBirthday()  V
-  // await MashovAPI.getHomework()  V
-  //await MashovAPI.getCards()      V
-  //await MashovAPI.getCardLinks()  V
-  //await MashovAPI.getTimetable()  V
-  //await MashovAPI.getMail()       V
-  //await MashovAPI.getRecipients() V
+  console.log(await MashovAPI.loginWithCreds({username:"",password:"",year:2021,school:441196}));
   console.log(await MashovAPI.getRecipients());
-  // let grades = await MashovAPI.getGrades();
-  // let average = 0;
-  // for(var i = 0; i < grades.length; i++){
-  //   average+=grades[i].grade;
-  // }
-  // average = average/grades.length;
-  // console.log("Your average is: " + average);
 })()
